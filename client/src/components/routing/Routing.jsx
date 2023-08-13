a8db11a8b7d171326f062e4af218dde4f16b08ad
Janwry Dela Cruz <jbenedicto20.jbb@gmail.com>
2023-08-13 18:30:07 +0800

    Setup tailwind

---
+++ client/src/components/routing/Routing.jsx
@@ -0,0 +1,36 @@
+import { React, useState, useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Login from '../Login';
+import Home from '../Home';
+
+const Routing = () => {
+//   const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 1440);
+
+//   useEffect(() => {
+//     // Function to set the isDesktop state based on the window width
+//     const handleWindowResize = () => {
+//       setIsDesktop(window.innerWidth >= 1440);
+//     };
+
+//     // Add event listener on component mount to handle window resize
+//     window.addEventListener('resize', handleWindowResize);
+
+//     // Clean up the event listener on component unmount
+//     return () => {
+//       window.removeEventListener('resize', handleWindowResize);
+//     };
+//   }, []);
+  return (
+    <>
+        <Routes>
+            {/* <Route path='/' element={<Home isDesktop={isDesktop} />} />
+            <Route path='/home' element={<Home isDesktop={isDesktop} />} />
+            <Route path='/work/view/:id' element={<ViewWork isDesktop={isDesktop} />} /> */}
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+        </Routes>
+    </>
+  )
+}
+
+export default Routing
