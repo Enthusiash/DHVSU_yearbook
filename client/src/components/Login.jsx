c5a44995ce95d3f43b07e14d52f9b72abc28bbee
Syntaxsh <ashleyotchengco@gmail.com>
2023-08-16 20:44:48 +0800

    Admin Login UI but still have bug in terms of logging in

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,11 +1,109 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+import http from './utils/http'
 
 const Login = () => {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await http.post("/auth", {username, password}) // Error here
+    
+    .then ((res) => {
+      console.log("Login Successful!");
+    })
+
+    .then ( () => {
+      localStorage.setItem("token", res.data)
+      window.location.href = "/"
+    })
+
+    .catch ((err) => {
+      alert(err.response.data);
+    })
+  }
+
   return (
     <div>
-      <h1>LOGIN</h1>
-    </div>
-  )
-}
 
-export default Login
+      <div className="bg-cover opacity-50 bg-no-repeat bg-center w-full h-screen absolute" style={{ backgroundImage: 'url(/src/assets/dhvsu-cover.jpg)', zIndex: "-1" }}></div>
+
+        <div className="h-screen flex justify-center items-center">
+
+          <div className="flex w-2/6 h-3/5 flex-col justify-center item-center bg-slate-50 rounded-[20px]">
+          
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+              <span className="flex justify-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24"><path fill="#4f46e5" d="M16.75 8.5a1.25 1.25 0 1 0 0-2.5a1.25 1.25 0 0 0 0 2.5Z"/><path fill="#4f46e5" d="M15.75 0a8.25 8.25 0 1 1-2.541 16.101l-1.636 1.636a1.744 1.744 0 0 1-1.237.513H9.25a.25.25 0 0 0-.25.25v1.448a.876.876 0 0 1-.256.619l-.214.213a.75.75 0 0 1-.545.22H5.25a.25.25 0 0 0-.25.25v1A1.75 1.75 0 0 1 3.25 24h-1.5A1.75 1.75 0 0 1 0 22.25v-2.836c0-.464.185-.908.513-1.236l7.386-7.388A8.249 8.249 0 0 1 15.75 0ZM9 8.25a6.733 6.733 0 0 0 .463 2.462a.75.75 0 0 1-.168.804l-7.722 7.721a.25.25 0 0 0-.073.177v2.836c0 .138.112.25.25.25h1.5a.25.25 0 0 0 .25-.25v-1c0-.966.784-1.75 1.75-1.75H7.5v-1c0-.966.784-1.75 1.75-1.75h1.086a.25.25 0 0 0 .177-.073l1.971-1.972a.75.75 0 0 1 .804-.168A6.75 6.75 0 1 0 9 8.25Z"/></svg>
+              </span>
+              <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                Sign in to your account
+              </h2>
+            </div>
+
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+              <form className="space-y-6" action="#" method="POST" onSubmit={handleSubmit}>
+                <div>
+                  <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                    Username:
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      onChange={(e) => setUsername(e.target.value)}
+                      id="username"
+                      name="username"
+                      type="username"
+                      autoComplete="username"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                      Password:
+                    </label>
+                    <div className="text-sm">
+                      <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                        Forgot password?
+                      </a>
+                    </div>
+                  </div>
+                  <div className="mt-2">
+                    <input
+                      onChange={(e) => setPassword(e.target.value)}
+                      id="password"
+                      name="password"
+                      type="password"
+                      autoComplete="current-password"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <button
+                    type="submit"
+                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Sign in
+                  </button>
+                </div>
+              </form>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default Login;
